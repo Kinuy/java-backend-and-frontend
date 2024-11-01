36ef03fa16ed21f09b010e9535de20a03303b8d5
Tobias Weber <toby-weber@gmx.de>
2024-11-01 18:30:38 +0100

    change todo items

--- frontend/src/TodoCard.tsx
+++ frontend/src/TodoCard.tsx
@@ -1,5 +1,7 @@
 import {Todo} from "./Todo.ts";
 import axios from "axios";
+import {useState} from "react";
+import * as React from "react";
 
 type Props = {
     todo:Todo,
@@ -8,14 +10,27 @@ type Props = {
 
 export default function TodoCard(props: Props) {
 
+    const [description, setDescription] = useState(props.todo.description)
+
     function deleteThisItem(){
         axios.delete("api/todo/"+props.todo.id)
             .then(props.onTodoItemChange)
     }
 
+    function changeText(event: React.ChangeEvent<HTMLInputElement> ){
+        let newDescription = event.target.value
+        setDescription(newDescription)
+        axios.put("api/todo/"+props.todo.id, {
+            id: props.todo.id,
+            description: newDescription,
+            status: props.todo.status,
+        } as Todo)
+    }
+
     return (
         <div className="todo-card">
-            {props.todo.description}
+
+            <input value={description} onInput={changeText}/>
             <button onClick={deleteThisItem}>delete</button>
         </div>
     );
